decb965ba67f18ba4e63d517b6e9ea2af6f79478
Adrian Rodrigo Melon Gutte <rodrigomelongutte@gmail.com>
2024-03-01 10:27:11 +0100

    Create new dev vpc

--- main.tf
+++ main.tf
@@ -18,14 +18,31 @@ data "aws_vpc" "default" {
   default = true
 }
 
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-north-1a", "eu-north-1b", "eu-north-1c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
   vpc_security_group_ids = [module.web_sg.security_group_id]
 
+  subnet_id = module.web_vpc.public_subnets[0]
+
   tags = {
-    Name = "HelloWorld"
+    Name = "Curso Terraform"
   }
 }
 module "alb" {
@@ -73,7 +90,7 @@ module "web_sg" {
   version = "5.1.0"
   name    = "web_new"
 
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = module.web_vpc.vpc_id
     
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
