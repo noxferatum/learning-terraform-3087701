ea6d8ac3459d60cd5d53753da796a5557e893f5e
Adrian Rodrigo Melon Gutte <rodrigomelongutte@gmail.com>
2024-03-01 10:13:58 +0100

    2

--- main.tf
+++ main.tf
@@ -28,11 +28,13 @@ resource "aws_instance" "web" {
     Name = "HelloWorld"
   }
 }
-
 module "alb" {
-  source = "terraform-aws-modules/alb/aws"
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 6.0"
 
-  name    = "web-alb"
+  name = "web-alb"
+
+  load_balancer_type = "application"
 
   vpc_id          = module.web_vpc.vpc_id
   subnets         = module.web_vpc.publics_subnets
@@ -44,25 +46,25 @@ module "alb" {
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
-      targets = {
-        my_target = {
+      targets = [
+        {
           target_id = aws-instance.web.id
-          port      = 80
+          port = 80
         }
-      }
+      ]
     }
   ]
 
   http_tcp_listeners = [
     {
-      port             = 80
-      protocol         = "HTTP"
-      target_group_key = 0
-      }
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
   ]
-    
+
   tags = {
-    Environment = "Dev"
+    Environment = "dev"
   }
 }
 
